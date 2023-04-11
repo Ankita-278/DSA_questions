2cd28a11851964985efb92afe5ef95737c2adb97
Ankita <ankita39640@gmail.com>
2023-04-11 23:38:14 +0530

    algorithm_stl

---
+++ STL-ALOG.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void explainextra(){
+    // sort(v.begin(),v.end());
+   sort(a+2,a+4);
+    sort(a,a+n);
+    int n=n.size();
+   pair<int,int>a[]={{1,2},{2,1},{4,1}};
+  // sort(a,a+n,comp);
+}
+int main(){
+    vector<int>a;
+    explainextra();
+    return 0;
+}
