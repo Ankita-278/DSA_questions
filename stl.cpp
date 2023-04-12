06da2896ed56cb7a68fa5289879b3bff66b21b19
Ankita <ankita39640@gmail.com>
2023-04-13 00:24:04 +0530

    code

--- stl.cpp
+++ stl.cpp
@@ -1,7 +1,21 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    
+    int x;
+  cin>>x;
+  string y;
+  map<string,int>m;
+  while(cin>>x>>y)
+      switch(x){
+      case 1:cin>>x;
+             m[y]+=x;
+             break;
+      case 2:
+             m.erase(y);
+             break;
+      case 3:
+            cout<<m[y]<<endl;
+  }
     return 0;
 }
