0e509c4b35c4ca942754522dc2ddf6438a648934
Ankita <ankita39640@gmail.com>
2023-04-16 11:24:28 +0530

    recursion questions

---
+++ recursion2.cpp
@@ -0,0 +1,16 @@
+//print linearly from 1 to n
+#include<iostream>
+using namespace std;
+
+void f(int i,int n){
+    if(i>n)
+    return;
+    cout<<i<<endl;
+    f(i+1,n);
+}
+int main(){
+    int n;
+    cin>>n;
+    f(1,n);
+    return 0;
+}
